795a91ac677f4b72b1bf1679a8968251a50a77ce
Christian Moyes <moyechri846@gmail.com>
2023-09-15 09:53:23 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Topic_4___Random_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random generator = new Random();
+
+        }
+    }
+}
